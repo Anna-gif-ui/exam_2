827f27c3435b7982d0cbd0feaff8b58126993c99
Anna-gif-ua <anna.gadiyeva@gmail.com>
2020-06-19 17:58:24 +0300

    added news gallery footer sections

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,24 @@ $(document).ready(function () {
         dots: true,
         // adaptiveHeight: true,
         infinite: true,
+  
         // autoplay: true,
         //   autoplaySpeed: 400,
-        speed: 3000,
+        speed: 4000,
         vertical: true,
         verticalSwiping: true,
         // swipeToSlide: true,
         slidesToShow: 1,
-        // slidesToScroll: 1,
+        slidesToScroll: 1,
      });
     
          $('.news-slider').slick({
             dots: true,
             arrows:true,
+            prevArrow   : `<button class="slider__arrow slider__arrow--prev btn btn--slide btn--icon">
+                        <i class="fas fa-chevron-left"></i></button>`,
+            nextArrow   : `<button class="slider__arrow slider__arrow--next btn btn--slide btn--icon">
+                         <i class="fas fa-chevron-right"></i></button>`,
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 1,
